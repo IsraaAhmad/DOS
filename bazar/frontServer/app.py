e7667ae028d74fa7b931e394e9c4d8e496f02eab
Wafaa Daas <w.daas.1999@hotmail.com>
2021-11-03 18:20:28 +0200

    information -front server-

--- bazar/frontServer/app.py
+++ bazar/frontServer/app.py
@@ -11,6 +11,20 @@ def search(topic):
     response = requests.get("http://192.168.1.30:5000/search/" + topic)
     return response.content
 
+# return all information about all books
+# this req send to catalog server 
+@app.route('/information/all', methods=['Get'])
+def information_all():
+    response = requests.get("http://192.168.1.30:5000/information/all")
+    return response.content
+
+# return all information about specific book according to given ID
+# this req send to catalog server 
+@app.route('/information/<int:id>', methods=['Get'])
+def information_id(id):
+    response = requests.get("http://192.168.1.30:5000/information/" + str(id))
+    return response.content
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=3500)
