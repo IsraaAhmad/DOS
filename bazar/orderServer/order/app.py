f3249b21e13957b95cd585d4fa0efd246a0cd697
Wafaa Daas <w.daas.1999@hotmail.com>
2021-11-06 19:28:44 +0200

    adding comments to order server

--- bazar/orderServer/order/app.py
+++ bazar/orderServer/order/app.py
@@ -3,11 +3,14 @@ import requests
 
 app = Flask(__name__)
 
-#redirect the purchase requset to decrese requsest
+# this req will check if there is enough books in the store and make the order
 @app.route('/purchase/<int:id>', methods=['Post'])
 def purchase(id):
    
     amount = request.form.get('amount')
+    #redirect the purchase requset to check if there is enough books if yes
+    #then it will dec the number of books and sent aprovel; if not it will reject
+    # the req
     response  = requests.put("http://192.168.1.30:5000/decrease/"+ str(id), {'amount':amount})
     
     x = response.json()
